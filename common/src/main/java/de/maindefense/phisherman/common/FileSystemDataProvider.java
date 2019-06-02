390d8739948a441414140e1d78ba99f396e4e79d
Martin Spielmann <mail@martinspielmann.de>
2019-06-03 01:05:33 +0200

    remove unused imports

--- common/src/main/java/de/maindefense/phisherman/common/FileSystemDataProvider.java
+++ common/src/main/java/de/maindefense/phisherman/common/FileSystemDataProvider.java
@@ -2,9 +2,7 @@ package de.maindefense.phisherman.common;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 public class FileSystemDataProvider {
 
