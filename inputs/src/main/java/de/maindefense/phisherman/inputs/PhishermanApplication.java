16e79a386429bedd7ffa57c6f99be2b4e68acdf2
Martin Spielmann <mail@martinspielmann.de>
2019-06-03 01:13:07 +0200

    Fix auto configuration

--- inputs/src/main/java/de/maindefense/phisherman/inputs/PhishermanApplication.java
+++ inputs/src/main/java/de/maindefense/phisherman/inputs/PhishermanApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"de.maindefense.phisherman"})
 @EnableConfigurationProperties
 public class PhishermanApplication {
 
--- outputs/src/main/java/de/maindefense/phisherman/outputs/PhishermanApplication.java
+++ outputs/src/main/java/de/maindefense/phisherman/outputs/PhishermanApplication.java
@@ -2,8 +2,10 @@ package de.maindefense.phisherman.outputs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"de.maindefense.phisherman"})
+@EnableConfigurationProperties
 public class PhishermanApplication {
 
 	public static void main(String[] args) {
